26101968cf813537b973aac7542dd152bf9b709e
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-04-27 08:00:08 -0700

    refactor: clean up

--- components/Smiley.tsx
+++ components/Smiley.tsx
@@ -1,14 +1,16 @@
-// 3 is low, 8 is high. 0 is forbidden
-const Smiley = ({ percentage = 0 }) => {
+const Smiley = ({ percentage = 50 }) => {
+  // Avoid forbidden 0
   if (percentage === 40) {
-    percentage = 41
+    percentage = 42
   }
+
+  // 3 is low, 8 is high. 0 is forbidden
   const calculatedTop = percentage / 20 + 3
 
   return (
     <svg height={100} viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-      <g data-name="Layer 2" id="Layer_2">
-        <g data-name="Layer 1" id="Layer_1-2">
+      <g>
+        <g>
           <circle cx="2" cy="2" r="1" />
           <circle cx="8" cy="2" r="1" />
           <path d={`M2,5 C2,${calculatedTop} 8,${calculatedTop} 8,5`} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
 import type { NextPage } from 'next'
-import Link from 'next/link'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      TODO
-      <Link href="/test">go to test page</Link>
-    </div>
-  )
+  return <div>TODO</div>
 }
 
 export default Home
