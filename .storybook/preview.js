6620994da832d5258d45880db50b775117f78222
miles-grant-ibigroup <miles.grant@ibigroup.com>
2022-05-13 13:49:59 -0700

    refactor: address pr feedback

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,3 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {},
-  },
+  actions: { argTypesRegex: "^on[A-Z].*" }
 }
--- components/QuestionWrapper.tsx
+++ components/QuestionWrapper.tsx
@@ -2,6 +2,9 @@ import styles from '../styles/QuestionWrapper.module.css'
 
 type Props = { children?: JSX.Element }
 
+/**
+ * This wrapper ensures that only one question is shown at a time.
+ */
 const QuestionWrapper = ({ children }: Props) => (
   <section className={styles.q}>{children}</section>
 )
